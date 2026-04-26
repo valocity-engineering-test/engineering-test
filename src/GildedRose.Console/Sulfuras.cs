5618e951c1e81f0455c91fcecf0d19b5fab7afd5
LokendraJayaswal <lokendrajayaswal@gmail.com>
2026-04-27 02:14:09 +0530

    Refactored Code Commit WIth Test Cases

---
+++ src/GildedRose.Console/Sulfuras.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseApp.Console
+{
+    /// <summary>
+    /// Sulfuras is a legendary item:
+    /// Quality is always 80 and never changes.
+    /// SellIn never decreases.
+    /// </summary>
+    public class Sulfuras : IItemBehavior
+    {
+        public void Update(Item item)
+        {
+            // Do nothing: Sulfuras never changes
+        }
+    }
+
+}
